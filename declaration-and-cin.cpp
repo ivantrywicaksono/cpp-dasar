77e340119d18f8db67a43e68c56cb3496439ee19
ivantrywicaksono <ivantrywicaksono04@gmail.com>
2023-06-28 00:14:27 +0700

    feat: add declaration-and-cin.cpp

---
+++ declaration-and-cin.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void)
+{
+    // Declaration
+    int a;
+
+    a = 10;
+    std::cout << a << std::endl;
+
+    // Declaration
+    int b;
+
+    std::cin >> b;
+    std::cout << b << std::endl;
+    
+    return 0;
+}
--- variable.cpp
+++ variable.cpp
@@ -4,7 +4,7 @@ int main(void)
 {
     int a = 5;
     std::cout << a << std::endl;
-    
+
     int x = 4;
     std::cout << x << std::endl;
     return 0;
